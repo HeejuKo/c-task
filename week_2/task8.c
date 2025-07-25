ebb1ae458e6a88a5016b75d10372dd4c80b7c028
HeejuKo <khjg00gle96@gmail.com>
2025-07-26 04:29:51 +0900

    2주차 과제(~8)

--- week_2/task8.c
+++ week_2/task8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -7,7 +8,6 @@ int main(void)
 
     // 창문이 건드려진 횟수 = 약수의 개수
     // 약수의 개수가 홀수 개인 수 = 제곱수
-    for (int i = 1; i <= N; i++)
-    {
-        }
+    int count = (int)sqrt(N);
+    printf("%d\n", count);
 }
---
+++ week_2/task9.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long long int A, B;
+    scanf("%d %d", &A, &B);
+}
