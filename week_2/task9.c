4e9e190b9c5884ce0ad0e19e99110f5b7393551f
HeejuKo <khjg00gle96@gmail.com>
2025-07-26 06:11:32 +0900

    2주차 과제

--- week_2/task9.c
+++ week_2/task9.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+long long gcd(long long a, long long b)
+{
+    while (b != 0)
+    {
+        long long temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+long long lcm(long long a, long long b)
+{
+    return a * b / gcd(a, b);
+}
+
 int main(void)
 {
-    long long int A, B;
-    scanf("%d %d", &A, &B);
+    long long A, B, gcd, res;
+    scanf("%lld %lld", &A, &B);
+
+    res = lcm(A, B);
+    printf("%lld\n", res);
 }
